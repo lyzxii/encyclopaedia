897220d14d4b1e940ecedc9bdccffe8965cbec24
yanpeidong <macintoshhd@192.168.1.9>
2023-09-04 23:59:19 +0800

    1

---
+++ encyclopaedia-facade/src/main/java/com/broccoli/encyclopaedia/EncyclopaediaApplication.java
@@ -0,0 +1,13 @@
+package com.broccoli.encyclopaedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncyclopaediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncyclopaediaApplication.class, args);
+	}
+
+}
